d42c5d737b6ba971de642c56fbd2673b7befb99b
Dawid Pers <dawid.g657@gmail.com>
2023-05-10 20:16:56 +0200

    Add project to Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <AppBar elevation={0} color="primary" variant="outlined">
+                <Toolbar>
+                    <Typography variant="h6">asd</Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/theme/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#18181a',
+        },
+        secondary: {
+            main: '#ac7410',
+        },
+    },
+});
+
+export default theme;
